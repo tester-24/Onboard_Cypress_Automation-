a32405992e33290125a6bc002772b1dc6184c191
Tester1 <137996855+tester-24@users.noreply.github.com>
2025-01-22 17:42:07 +0530

    Onboard_Reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
 projectId: "jab7s8",
+reporterOptions: {
+   
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    enableCode:false,
+    embeddedvideos: true,
+    video: true,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
